91e989c0ed1cc09119b476b5a5039ae0d7f5e1ee
jedrzejsekowski <jedrzej.sekowski@gmail.com>
2024-09-01 23:13:23 +0200

    slowdown scraping to 1 req per sec

--- timocom/spiders/freights.py
+++ timocom/spiders/freights.py
@@ -1,8 +1,10 @@
 import json
 import scrapy
+import time 
 from datetime import datetime
 from ..items import TimocomItem
 from scrapy.loader import ItemLoader
+
 
 origins = ['AL','AM','AT','AZ','BA','BE','BG','BY','CH','CY','CZ','DE','DK','EE','ES','EU','FI','FR','GB','GE','GR','HR','HU','IE','IS','IT','KZ','LI','LT','LU','LV','MC','MD','ME','MK','MT','NL','NO','PL','PT','RO','RS','RU','SE','SI','SK','TR','UA']
 destinations = ['AL','AM','AT','AZ','BA','BE','BG','BY','CH','CY','CZ','DE','DK','EE','ES','EU','FI','FR','GB','GE','GR','HR','HU','IE','IS','IT','KZ','LI','LT','LU','LV','MC','MD','ME','MK','MT','NL','NO','PL','PT','RO','RS','RU','SE','SI','SK','TR','UA']
@@ -13,6 +15,9 @@ class FreightsSpider(scrapy.Spider):
     def start_requests(self):
         for origin in origins:
             for destination in destinations:
+                
+                time.sleep(1) # Slowdown scraping to 1 reqs/sec
+                
                 yield scrapy.Request(
                     url="https://services.timocom.com/api/barometerData",
                     method="POST",
